ef8ea864aaa4974fc21a7076ad2d2b10d23211ad
giova <giova@DESKTOP-DH2NOAC>
2022-08-22 23:15:13 -0300

    Agregar archivos de proyecto.

---
+++ Anotaciones [Clase 1]/Program.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name;
+        string ageString;
+        int age;
+        bool conversionResult;
+
+        // Arrays
+
+        string[] nombres = new string[3];
+        nombres[0] = "Gianni";
+        nombres[1] = "Giovanni";
+        nombres[2] = "Esteban";
+
+        for (int i = 0; i < nombres.Length; i++)
+        {
+            Console.WriteLine(nombres[i]);
+        }
+
+        string[] paises = { "\nBrasil", "Argentina", "Chile" }; // El limite está seteado por los valores dentro de las llaves
+
+        foreach (string pais in paises)
+        {
+            Console.WriteLine(pais);
+        }
+
+        // Arrays
+
+        Console.WriteLine("\nIngrese su nombre");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Ingrese su edad");
+        ageString = Console.ReadLine();
+
+
+        // 1era manera de parsear haciendo que podamos obtener un booleano para comprobar la conversión.
+        conversionResult = int.TryParse(ageString, out age);
+        Console.WriteLine("Se pudo convertir? {0}", conversionResult);
+
+        // 2da manera de parsear no contemplando si falla o no.
+        age = int.Parse(ageString);
+
+
+        //name = Console.ReadLine();
+
+        Console.WriteLine("Su edad es: " + age + " Y su nombre es : " + name);
+
+        // interpolado
+        Console.WriteLine($"La edad es: {age}");
+
+        if (conversionResult != false)
+        {
+            Console.WriteLine("Su nombre es: {0} y su edad es: {1}!", name, age);
+        }
+
+        // Especificadores de formato precio:C2 agrega un $
+    }
+}
---
+++ Ejercicio I01 - Máximo, mínimo y promedio/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+// Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.
+// Giovanni Lucchetta
+
+namespace Ejercicio_I01___Máximo__mínimo_y_promedio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int enteredNumber;
+            int acumulator;
+            int highestNumber;
+            int lowestNumber;
+            int averageNumber;
+
+            highestNumber = 0;
+            lowestNumber = 0;
+            acumulator = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine("Enter a number please: ");
+                enteredNumber = int.Parse(Console.ReadLine());
+
+                if (i == 0)
+                {
+                    highestNumber = enteredNumber;
+                    lowestNumber = enteredNumber;
+                }
+                else
+                {
+                    if (highestNumber < enteredNumber)
+                    {
+                        highestNumber = enteredNumber;
+                    }
+                    else
+                    {
+                        if (lowestNumber > enteredNumber)
+                        {
+                            lowestNumber = enteredNumber;
+                        }
+                    }
+                }
+
+                acumulator += enteredNumber;
+            }
+
+            averageNumber = acumulator / 5;
+
+            Console.WriteLine("El valor maximo es: {0}\n El valor minimo es: {1}\n El promedio es: {2}", highestNumber, lowestNumber, averageNumber);
+            Console.WriteLine($"El valor maximo es: {highestNumber}\n El valor minimo es: {lowestNumber}\n El promedio es: {averageNumber}");
+
+
+        }
+    }
+}
