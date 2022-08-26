ff9c84cb3ec3cf6084c4a5458ecfc9454626d653
GioLucc <giolucarna@gmail.com>
2022-08-26 18:45:08 -0300

    Feat :sparkles: - Done and tested excercise 3-2!

---
+++ BibliotecaDeClases/Persona.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDeClases
+{
+    public class Persona
+    {
+       public string nombre;
+       public string fechaDeNacimiento;
+       public int dni;
+       public int edadActual;
+
+        public Persona(string nombre, string fechaDeNacimiento, int dni)
+        {
+            this.nombre = nombre;
+            this.fechaDeNacimiento = fechaDeNacimiento;
+            this.dni = dni;
+        }
+
+        public int SetNombre(string nombre)
+        {
+            int retorno = 0;
+
+            if(nombre is not null)
+            {
+                this.nombre = nombre;
+                retorno = 1;
+            }
+
+            return retorno;
+        } 
+        public int SetFechaDeNacimiento(string fechaDeNacimiento)
+        {
+            int retorno = 0;
+
+            if(fechaDeNacimiento is not null)
+            {
+                this.fechaDeNacimiento = fechaDeNacimiento;
+                retorno = 1;
+            }
+
+            return retorno;
+        }
+        public int SetDNI(int dni)
+        {
+            int retorno = 0;
+
+            if(dni > 0)
+            {
+                this.dni = dni;
+                retorno = 1;
+            }
+
+            return retorno;
+        }
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+
+        public string GetFechaDeNacimiento()
+        {
+            return fechaDeNacimiento;
+        }
+
+        public int GetDNI()
+        {
+            return dni;
+        }
+
+        internal double CalcularEdad(string fechaNacimientoRecibida)
+        {
+            DateTime fechaNacimiento;
+            DateTime fechaActual;
+            TimeSpan diferenciaDeDias;
+            double diasDeVida;
+            double añosDeEdad;
+
+            fechaNacimiento = DateTime.ParseExact(fechaDeNacimiento, "dd/MM/yyyy", null);
+            fechaActual = DateTime.Now;
+
+            diferenciaDeDias = fechaActual - fechaNacimiento;
+            diasDeVida = diferenciaDeDias.TotalDays;
+            añosDeEdad = Math.Floor(diasDeVida / 365);
+
+            return añosDeEdad;
+        }
+
+        public string MostrarDatos()
+        {
+           return $"El nombre de la persona es: {nombre}, su edad es: {CalcularEdad(fechaDeNacimiento)}, su DNI es: {dni}! ";
+        }
+
+        public string EsMayorDeEdad()
+        {
+            string esMayor;
+
+            esMayor = "No es mayor de edad";
+
+            if(CalcularEdad(fechaDeNacimiento) > 17)
+            {
+                esMayor = "Es mayor de edad";
+            }
+            return esMayor;
+        }
+
+
+    }
+}
---
+++ Ejercicio I02 - Vos cuántas primaveras tenés/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using BibliotecaDeClases;
+
+/* 
+    Crear una aplicación de consola y una biblioteca de clases que contenga la clase Persona.
+
+    Deberá tener los atributos:
+
+    nombre
+    fechaDeNacimiento
+    dni
+    Deberá tener un constructor que inicialice todos los atributos.
+
+    Construir los siguientes métodos para la clase:
+
+    Setter y getter para cada uno de los atributos.
+    CalcularEdad será privado y retornará la edad de la persona calculándola a partir de la fecha de nacimiento.
+    Mostrar retornará una cadena de texto con todos los datos de la persona, incluyendo la edad actual.
+    EsMayorDeEdad si es mayor de edad devuelve el valor “Es mayor de edad", sino devuelve “es menor”.
+    Instanciar 3 objetos de tipo Persona en el método Main.
+    Mostrar quiénes son mayores de edad y quiénes no.
+
+*/
+
+namespace Ejercicio_I02___Vos_cuántas_primaveras_tenés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona personaUno = new Persona("Silvia", "08/08/1962", 19898234);
+            Persona personaDos = new Persona("Eride", "25/09/1935", 19754756);
+            Persona personaTres = new Persona("Stefano", "15/02/2010", 12321321);
+
+            Console.WriteLine(personaUno.MostrarDatos());
+            Console.WriteLine(personaDos.MostrarDatos());
+            Console.WriteLine(personaTres.MostrarDatos());
+
+            Console.WriteLine(personaTres.EsMayorDeEdad());
+            Console.WriteLine(personaUno.EsMayorDeEdad());
+
+
+
+        }
+    }
+}
