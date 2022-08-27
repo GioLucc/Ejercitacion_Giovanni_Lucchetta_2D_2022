22a1c1030a68ee83236aec0ce653237642b08733
GioLucc <giolucarna@gmail.com>
2022-08-27 04:50:32 -0300

    Feat :sparkles: - Excercise 3-3 passed all tests!

---
+++ BibliotecaDeClases/Estudiante.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BibliotecaDeClases
+{
+    public class Estudiante
+    {
+        private string apellido;
+        private string legajo;
+        private string nombre;
+        private int notaPrimerParcial;
+        private int notaSegundoParcial;
+
+        //Crear atributo privado no pude
+        // No se como hacer el constructor estatico
+        // Porque no se puede inicializar acá y solo se puede ne la función
+       
+        public Estudiante(string apellido, string legajo, string nombre)
+        {
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nombre = nombre;
+        }
+
+        public void setNotaPrimerParcial(int nota)
+        {
+            if(nota > 0)
+            {
+                notaPrimerParcial = nota;
+
+            }
+        }
+        public void setNotaSegundoParcial(int nota)
+        {
+            if(nota > 0)
+            {
+                notaSegundoParcial = nota;
+            }
+        }
+
+        private float CalcularPromedio()
+        {
+            int sumaDeNotas;
+            float promedio;
+            promedio = 0;
+
+            if (notaPrimerParcial > 0 && notaPrimerParcial > 0)
+            {
+                sumaDeNotas = notaPrimerParcial + notaSegundoParcial;
+                promedio = sumaDeNotas / 2;
+            }
+            return promedio;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            double notaFinal;
+            notaFinal = -1;
+            Random rand = new Random();
+
+            if(notaPrimerParcial > 3 && notaSegundoParcial > 3)
+            {
+                notaFinal = rand.Next(4, 11);
+            }
+
+            return notaFinal;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append($"El nombre del alumno es: {nombre}.\n El apellido del alumno es: {apellido}.\nEl Legajo del alumno es: {legajo}.\n");
+            sb.Append($"La nota de primer parcial es: {notaPrimerParcial} y la nota del segundo parcial es: {notaSegundoParcial}");
+            if(CalcularNotaFinal() != -1)
+            {
+                sb.Append($"\n\nLa nota del FINAL ES: {CalcularNotaFinal()}\n\n");
+
+            }
+            else
+            {
+                sb.Append($"\n\nAlumno desaprobado\n\n");
+            }
+
+            return sb.ToString();
+        }
+
+        
+
+
+    }
+
+}
---
+++ Ejercicio I03 - El ejemplo universal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BibliotecaDeClases;
+/*
+    La clase Estudiante:
+
+    Tendrá un constructor estático que inicializará el atributo estático random.
+    Tendrá un constructor de instancia que inicializará los atributos nombre, apellido y legajo.
+    El método setter SetNotaPrimerParcial permitirá cambiar el valor del atributo notaPrimerParcial.
+    El método setter SetNotaSegundoParcial permitirá cambiar el valor del atributo notaSegundoParcial.
+    El método privado CalcularPromedio retornará el promedio de las dos notas.
+    El método CalcularNotaFinal deberá retornar la nota del final con un numero aleatorio entre 6 y 10 incluidos siempre y cuando las notas del primer y segundo parcial sean mayores o iguales a 4, caso contrario la inicializará con el valor -1.
+    El método Mostrar utilizará StringBuilder para armar una cadena de texto con todos los datos de los alumnos:
+    Nombre, apellido y legajo.
+    Nota del primer y segundo parcial.
+    Promedio.
+    Nota final. Se mostrará sólo si el valor es distinto de -1, caso contrario se mostrará la leyenda "Alumno desaprobado".
+    Crear tres instancias de la clase Estudiante (tres objetos) en el método Main.
+    Cargar las notas del primer y segundo parcial a todos los alumnos. Dos deberán estar aprobados y uno desaprobado.
+    Mostrar los datos de todos los alumnos. 
+
+ */
+
+namespace Ejercicio_I03___El_ejemplo_universal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante primerEstudiante = new Estudiante("Lucchetta","142233","Giovanni");
+            Estudiante segundoEstudiante = new Estudiante("Mejias", "142535", "Thiago");
+            Estudiante tercerEstudiante = new Estudiante("Taboada", "142434", "Ezequiel");
+
+            primerEstudiante.setNotaPrimerParcial(6);
+            primerEstudiante.setNotaSegundoParcial(10);
+            
+            segundoEstudiante.setNotaPrimerParcial(6);
+            segundoEstudiante.setNotaSegundoParcial(10);
+
+            tercerEstudiante.setNotaPrimerParcial(4);
+            tercerEstudiante.setNotaSegundoParcial(3);
+
+            Console.WriteLine(primerEstudiante.Mostrar());
+            Console.WriteLine(segundoEstudiante.Mostrar());
+            Console.WriteLine(tercerEstudiante.Mostrar());
+        }
+    }
+}
